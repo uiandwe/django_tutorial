dc925293e75cfde50f2a983e18ff97d823a1f29e
hyeon seung jae <uiandwe@gmail.com>
2018-12-27 17:41:47 +0900

    static 폴더 추가 및 뷰 변경

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
+
 def index(request):
-    latest_question_list = ["test", "test1"]
+    latest_question_list = ["데드풀1", "어벤져스", "슈퍼맨vs배트맨", "데드풀2"]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'movies/index.html', context)
 
