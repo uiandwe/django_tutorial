bd510e40825f899a80d63d8c166f860a77b24bfc
hyeon seung jae <uiandwe@gmail.com>
2018-12-30 23:50:24 +0900

    util user check 추가

--- movies/decorators.py
+++ movies/decorators.py
@@ -1,8 +1,10 @@
 import functools
+from . import utils
 
 def decorator(view_func):
     @functools.wraps(view_func)
     def new_view_func(request, *args, **kwargs):
+        request = utils.check_rights(request)
         response = view_func(request, *args, **kwargs)
         return response
     return new_view_func
---
+++ movies/utils.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import PermissionDenied
+
+
+def check_rights(request):
+    if request.user.is_anonymous is True:
+        return request
+
+    raise PermissionDenied
