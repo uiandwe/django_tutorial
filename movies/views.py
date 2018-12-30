e17751ddff14f5ba0f4ef604da0a1a4caf853731
hyeon seung jae <uiandwe@gmail.com>
2018-12-30 23:22:24 +0900

    decorator 추가

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
+from .decorators import decorator
 
+@decorator
 def question(request):
     latest_question_list = ["데드풀1", "어벤져스", "슈퍼맨vs배트맨", "데드풀2"]
     context = {'latest_question_list': latest_question_list}
