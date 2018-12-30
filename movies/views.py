a135792b2b933adc6cc6c6e827bfffcf79369c7b
hyeon seung jae <uiandwe@gmail.com>
2018-12-30 22:27:33 +0900

    템플릿 extends 추가

--- movies/views.py
+++ movies/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def index(request):
     latest_question_list = ["데드풀1", "어벤져스", "슈퍼맨vs배트맨", "데드풀2"]
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'movies/index.html', context)
+    return render(request, 'movies/question.html', context)
 
