1702c48cb4cb41a2f002a5f3168d5a0bbaae1a7e
hyeon seung jae <uiandwe@gmail.com>
2018-12-30 23:09:36 +0900

    movies 모델 수정

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+from core.models import FileModel
+
+class Movies(FileModel):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
