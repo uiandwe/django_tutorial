beb891cca4d89034fba5349bfa23f0e2a6a16dd7
hyeon seung jae <uiandwe@gmail.com>
2019-01-04 16:49:02 +0900

    post_servier pagenation 추가

--- post_service/admin.py
+++ post_service/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from post_service.models import Post
 
-# Register your models here.
+
+admin.site.register(Post)
--- post_service/views.py
+++ post_service/views.py
@@ -4,10 +4,24 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.template import Context
 from django.template.loader import get_template
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
+from post_service.models import Post
 
 def post_list(request):
     template = get_template('post_list.html')
-    context = { 'python_world' : '시이발'}
+
+    page_data = Paginator(Post.objects.all(), 5)
+    page = request.GET.get("page")
+
+
+    try:
+        posts = page_data.page(page)
+    except PageNotAnInteger:
+        posts = page_data.page(1)
+    except EmptyPage:
+        posts = page_data.page(page_data.num_pages)
+
+    context = {'post_list': posts}
 
     return HttpResponse(template.render(context))
