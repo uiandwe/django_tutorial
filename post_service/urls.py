c96e66554c869a42b4da8fb9d307e230ce522e1e
hyeon seung jae <uiandwe@gmail.com>
2019-01-05 18:33:07 +0900

    로그인 폼 수정

--- post_service/urls.py
+++ post_service/urls.py
@@ -3,12 +3,16 @@ from django.urls import path
 from . import views
 from django.conf.urls import url
 
-from post_service.views import post_list
+from post_service.views import post_list, login, login_validate
+
 
 
 app_name = 'post'
 urlpatterns = [
-    url(r'^$', post_list)
+    url(r'^$', post_list),
+    url(r'^login/validate', login_validate),
+    url(r'^login/', login),
+
     # path('', views.IndexView.as_view(), name='index'),
     # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
