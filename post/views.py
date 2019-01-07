7f20be0815a5e5a7598752207816a38f3fe12e74
hyeon seung jae <uiandwe@gmail.com>
2019-01-07 13:00:55 +0900

    reshape get -> put/del

--- post/views.py
+++ post/views.py
@@ -8,7 +8,12 @@ class PostList(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+# 오직 get / gets 만
+# class PostDetail(generics.RetrieveAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveAPIView):
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
