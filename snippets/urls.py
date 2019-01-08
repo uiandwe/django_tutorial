b51873891a8bfcfadc7483d39a94ca2f0f2ae0e1
hyeon seung jae <uiandwe@gmail.com>
2019-01-09 01:54:35 +0900

    class view 로 전환

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path('', views.snippet_list),
-    path('(?P<pk>[0-9]+)/', views.snippet_detail),
+    url(r'^$', views.SnippetList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
