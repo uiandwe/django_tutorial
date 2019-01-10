057bbaf56901b484f6737499a1923f3a1c65edff
hyeon seung jae <uiandwe@gmail.com>
2019-01-10 13:38:43 +0900

    user 세션 추가

--- user_manager/views.py
+++ user_manager/views.py
@@ -15,20 +15,18 @@ def login(request):
     template = get_template('login_form.html')
     context = {'login_form' : LoginForm()}
     context.update(csrf(request))
-
     return HttpResponse(template.render(context))
 
 
 def login_validate(request):
     login_form_data = LoginForm(request.POST)
-
     if login_form_data.is_valid():
         user = auth.authenticate(username=login_form_data.cleaned_data['id'], password=login_form_data.cleaned_data['password'])
 
         if user is not None:
             if user.is_active:
                 auth.login(request, user)
-
+                request.session.set_expiry(86400)
                 return redirect('/board/')
 
         else:
