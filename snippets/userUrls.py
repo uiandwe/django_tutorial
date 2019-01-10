049eded883724c1902b682b83c80f63955aae2a8
hyeon seung jae <uiandwe@gmail.com>
2019-01-10 13:38:26 +0900

    snippets 유저 로직 분리

---
+++ snippets/userUrls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+
+urlpatterns = [
+    url(r'^$', views.UserList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
