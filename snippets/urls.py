23c78c47328afbca0bc3a22562237f88f7330d77
hyeon seung jae <uiandwe@gmail.com>
2019-01-10 13:38:12 +0900

    snippets 유저 연동 추가

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
+from django.conf.urls import include
+from django.urls import path
+
 
 urlpatterns = [
-    url(r'^$', views.SnippetList.as_view()),
-    url(r'^(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    path('', views.SnippetList.as_view()),
+    path('<int:pk>/', views.SnippetDetail.as_view()),
+    # url('', views.SnippetList.as_view()),
+    # url('<int:pk>/', views.SnippetDetail.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
