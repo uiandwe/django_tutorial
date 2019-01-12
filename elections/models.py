a4c1a24144f5cc0ab285aa58830ad63928872dfd
hyeon seung jae <uiandwe@gmail.com>
2019-01-12 22:04:48 +0900

    Candidate model 추가

--- elections/models.py
+++ elections/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+
 class Candidate(models.Model):
     name = models.CharField(max_length=10)
     introduction = models.TextField()
     area = models.CharField(max_length=15)
     party_number = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name  # object를 출력하면 name이 보입니다.
