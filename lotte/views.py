017569117f6091e12cb6bb29400b49debf435ad2
hyeon seung jae <uiandwe@gmail.com>
2019-01-20 07:24:53 +0900

    view class -> def 수정 (serializer 및 함수 최소화를 위한 함수로 교체)

--- lotte/views.py
+++ lotte/views.py
@@ -3,33 +3,38 @@ from rest_framework import generics
 from .models import Lotte
 from .serializers import LotteSerializer
 import random
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 class LotteList(generics.ListCreateAPIView):
     queryset = Lotte.objects.all()
     serializer_class = LotteSerializer
 
 
-class LotteNumber(generics.ListAPIView):
-    queryset = Lotte.objects.all()
-    number_array = [[x, 0] for x in range(46)]
-    values = ["number1", "number2", "number3", "number4", "number5", "number6"]
-    for lotteObj in list(queryset):
-        for i in range(6):
-            lotteNum = getattr(lotteObj, values[i])
-            number_array[lotteNum][1] += 1
-    number_array = sorted(number_array, key=lambda l:l[1], reverse=True)
+@api_view()
+def LotteNumber(request):
+    # queryset = Lotte.objects.all()
+    # number_array = [[x, 0] for x in range(46)]
+    # values = ["number1", "number2", "number3", "number4", "number5", "number6"]
+    # for lotteObj in list(queryset):
+    #     for i in range(6):
+    #         lotteNum = getattr(lotteObj, values[i])
+    #         number_array[lotteNum][1] += 1
+    # number_array = sorted(number_array, key=lambda l:l[1], reverse=True)
 
-    numbers = [x for x in range(1, 47)]
-    random.shuffle(numbers)
     dict = {}
     index = 1
     while index <= 5:
-        number_set_filter = ','.join(str(e) for e in numbers[0:6])
+        numbers = [x for x in range(1, 47)]
+        random.shuffle(numbers)
+        number_set_filter = sorted(numbers[0:6])
+        number_set_filter = ','.join(str(e) for e in number_set_filter)
         number_set_count = Lotte.objects.filter(number_set=number_set_filter).count()
         if number_set_count == 0:
             dict[index] = number_set_filter
             index += 1
 
-    print(dict)
+    return Response(dict)
 
-    serializer_class = LotteSerializer
